019d93290e5bc4649f0ae630cc375684765d07b7
tpho216 <tpho216@gmail.com>
2021-10-10 18:29:45 +1100

    updated cors enable

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // ! need this to query data from another domain
+  app.enableCors();
+
   const document = SwaggerModule.createDocument(app, new DocumentBuilder()
     .setTitle('Portfolio API')
     .setDescription('API for portfolio data queries').build());
