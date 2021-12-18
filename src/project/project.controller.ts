06aef47ccef87fcc671e7ad4715ee231455dc3ed
tpho216 <tpho216@gmail.com>
2021-12-18 20:50:12 +1100

    updated CRUD operations for projects

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,40 +1,83 @@
 // project.controller.ts
 
-import { Body, Controller, Delete, Get, HttpStatus, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, UseFilters } from "@nestjs/common";
 import { ProjectService } from "./project.service";
 import { Project } from "../model/project.entity";
 import { ProjectDTO } from "./project.dto";
-import { ApiOperation } from "@nestjs/swagger";
+import { ApiBody, ApiHeader, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { HttpExceptionFilter } from "../utils/ExceptionFilters";
 
+
+@ApiTags('Projects')
+@ApiHeader({
+  name: 'Projects',
+})
 @Controller('project')
 export class ProjectController {
   constructor(private serv: ProjectService) {
   }
 
   @Get()
+  @ApiOperation({summary:'get all projects'})
   public async getAll() {
     return await this.serv.getAll();
   }
+  @Delete('All')
+  @ApiOperation({summary: 'delete all projects'})
+  public async deleteAll() {
+      try {
+        await this.serv.destroyAll();
+        return {
+          statusCode: HttpStatus.OK,
+          message: "All projects deleted"
+        }
+      }
+      catch (e) {
+        return {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: e
+        }
+      }
 
-  @Post()
-  @ApiOperation({ summary: 'insert project to current projects'})
-  public async post(@Body() projectDTO: ProjectDTO) : Promise<ProjectDTO> {
-    try {
-      // const dummyProjDTO = new ProjectDTO();
-      // dummyProjDTO.id = "8d91a8e9-bd9b-4e68-8678-d4a2519d456b";
-      // dummyProjDTO.name = "randomName";
-      // dummyProjDTO.description = "randomDesc";
-      //return dummyProjDTO;
-      return this.serv.create(projectDTO);
-     }
-    catch (e) {
-        console.log(e);
-        throw new e;
-     }
   }
 
+
+  //individual project operations
+  @Post()
+  @ApiOperation({ summary: 'insert a project to current projects'})
+  public async post(@Body() projectDTO: ProjectDTO) : Promise<ProjectDTO> {
+    try {
+      return this.serv.create(projectDTO);
+    }
+    catch (e) {
+      console.log(e);
+      throw new e;
+    }
+  }
+
+  @Put(':id')
+  @ApiOperation({summary:"replace a project name & description given the project id"})
+  @UseFilters(HttpExceptionFilter)
+  async replaceProject(@Param('id') id: string, @Body() project : ProjectDTO) {
+    try {
+      await this.serv.replace(id, project);
+      return {
+        statusCode: HttpStatus.OK,
+        message: "Project replaced successfully"
+      };
+    }
+    catch (e) {
+      console.log(e);
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: e
+      }
+    }
+  }
+
+
   @Delete(':id')
-  @ApiOperation({ summary: 'delete project from current projects given the project id'})
+  @ApiOperation({ summary: `delete project from current projects given the project id`})
   public async delete(@Param('id') id: string) {
     try {
         await this.serv.destroy(id);
@@ -44,7 +87,10 @@ export class ProjectController {
         };
     }
     catch (e) {
-
+      return {
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: e
+      }
     }
   }
 
