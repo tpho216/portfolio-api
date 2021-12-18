bc09549e84032b116d810abbab3dd774769ba575
tpho216 <tpho216@gmail.com>
2021-12-18 20:50:59 +1100

    removed redundant commented codes

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,10 +1,11 @@
 //project.service.ts
 
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable, Param } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Project } from '../model/project.entity';
 import { Repository } from 'typeorm';
 import { ProjectDTO } from "./project.dto";
+import { ApiHeaders, ApiTags } from "@nestjs/swagger";
 
 @Injectable()
 export class ProjectService {
@@ -20,19 +21,34 @@ export class ProjectService {
 
   public async create(object: any) : Promise<ProjectDTO> {
     try {
-      // const dummyProj = new Project();
-      // dummyProj.name = "randomName";
-      // dummyProj.description = "randomDesc";
-      // const result = await this.repo.save(dummyProj);
       const projEntity = ProjectDTO.toEntity(object);
       return this.repo.save(projEntity).then(e => {
-          return ProjectDTO.fromEntity(e)
-        })
+          return ProjectDTO.fromEntity(e);
+        },
+        )
 
     }
     catch (e) {
       console.log(e);
       throw e;
+    }
+  }
+
+  public async destroyAll() {
+    await this.repo.query(`DELETE FROM project;`);
+  }
+
+  //individual project operations
+
+  public async replace(id: string, newProj : ProjectDTO) {
+    try {
+      await this.repo.query(`UPDATE project SET name = '${newProj.name}'
+                                , description = '${newProj.description}'
+                                where id = '${id}'`);
+
+    }
+    catch (e) {
+      console.log(e);
     }
   }
 
